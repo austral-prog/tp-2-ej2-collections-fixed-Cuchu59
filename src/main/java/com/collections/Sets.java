c3deb5f7784948032253bc757c15ab9c79fa4778
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-08-28 19:06:44 -0300

    Update Sets.java

--- src/main/java/com/collections/Sets.java
+++ src/main/java/com/collections/Sets.java
@@ -9,5 +9,12 @@ public class Sets {
     private static final Set<String> ALCOHOLS = new HashSet<>(Arrays.asList("whiskey", "whisky", "white rum", "dark rum", "bourbon", "rye", "scotch", "vodka",
             "tequila", "gin", "dry vermouth", "sweet vermouth", "prosecco","aperol", "brandy", "mezcal",
             "triple sec", "coffee liqueur", "almond liqueur", "champagne", "orange curacao", "rum"));
-
+    public String check_drinks(String bebida, List<String> lista_ing) {
+        for (String ing : lista_ing) {
+            if (Sets.ALCOHOLS.contains(ing)) {
+                return bebida + " Cocktail";
+            }
+        }
+        return bebida + " Mocktail";
+    }
 }

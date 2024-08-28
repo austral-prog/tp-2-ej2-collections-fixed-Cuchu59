a9bffc3572ba00fd082f7988f0f5f029e4b61851
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-08-28 19:11:17 -0300

    Update Lists.java

--- src/main/java/com/collections/Lists.java
+++ src/main/java/com/collections/Lists.java
@@ -3,7 +3,7 @@ package com.collections;
 import java.util.List;
 
 public class Lists {
-    public int index_of(String word, List<String> lista) {
+    public int indexOf(String word, List<String> lista) {
         int count = 0;
         for (String elem : lista) {
             if(elem.equals(word)) {
@@ -14,7 +14,7 @@ public class Lists {
         return -1;
     }
 
-    public int index_of_by_index(String word, List<String> lista, int num) {
+    public int indexOfByIndex(String word, List<String> lista, int num) {
         int count = num;
         for (int i = num; i < lista.size(); i++) {
             if(lista.get(i).equals(word)) {
@@ -25,27 +25,26 @@ public class Lists {
         return -1;
     }
     
-    public int index_of_empty(List<String> lista) {
-        return index_of("", lista);
+    public int indexOfEmpty(List<String> lista) {
+        return indexOf("", lista);
     }
 
     public int put(String word, List<String> lista) {
-        int blank_i = index_of("", lista);
+        int blank_i = indexOf("", lista);
         if(blank_i != -1) {
             lista.set(blank_i, word);
         }
         
-        return index_of(word, lista);
+        return indexOf(word, lista);
     }
 
     public int remove(String word, List<String> lista) {
-        int blank_i = index_of(word, lista);
+        int blank_i = indexOf(word, lista);
         if(blank_i != -1) {
             lista.remove(blank_i);
         }
         
-        return index_of(word, lista);
+        return indexOf(word, lista);
     }
-
 
 }
